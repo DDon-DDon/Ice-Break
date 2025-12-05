1f3cabc67a3a5e2c9c9722b041baee5978af4100
isakggong <40036437+Isak-toast@users.noreply.github.com>
2025-12-05 22:52:57 +0900

    fix: 틀린 부분 찾아 수정

--- iskim/broken-translator/src/App.jsx
+++ iskim/broken-translator/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import TranslationInput from './components/TranslationInput';
-THIS_WILL_CAUSE_BUILD_ERROR!!!  // <-- 빌드 실패 유발!
 import TranslationChain from './components/TranslationChain';
 import ComparisonResult from './components/ComparisonResult';
 import { runTranslationChain } from './services/translationService';
